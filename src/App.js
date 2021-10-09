7e9d5ab0714266e89e59840fbf3c4f8d2bcc3f76
lincodes <83891163+lincodes@users.noreply.github.com>
2021-10-09 13:44:56 +0900

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>React Weather App</h1>
+      </div>
+    </div>
+  );
 }
